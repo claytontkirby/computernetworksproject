15a28dd11961397af4cb097dd7eeae412bb2e1ba
JamesKellerman <jfk535@mst.edu>
2014-12-02 19:50:04 -0600

    Updated binary file paths

--- main_folder/test_client_snd.ex
+++ main_folder/test_client_snd.ex
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-x-terminal-emulator -e test_clients/client.out snd & wait
+x-terminal-emulator -e client.out snd & wait
 
-x-terminal-emulator -e test_clients/client.out snd & wait
+x-terminal-emulator -e client.out snd & wait
 
-x-terminal-emulator -e test_clients/client.out snd & wait
+x-terminal-emulator -e client.out snd & wait
 
-x-terminal-emulator -e test_clients/client.out snd & wait
+x-terminal-emulator -e client.out snd & wait
 
-x-terminal-emulator -e test_clients/client.out snd & wait
+x-terminal-emulator -e client.out snd & wait
