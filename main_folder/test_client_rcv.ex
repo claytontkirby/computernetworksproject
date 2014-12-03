7f1b304b99a4487905e7e2fb46d57de765f5ef46
JamesKellerman <jfk535@mst.edu>
2014-12-03 16:20:50 -0600

    Updated client scripts

--- main_folder/test_client_rcv.ex
+++ main_folder/test_client_rcv.ex
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-x-terminal-emulator -e ./client.out rcv & wait
+x-terminal-emulator -e ./client.out rcv 6 & wait
 
-x-terminal-emulator -e ./client.out rcv & wait
+x-terminal-emulator -e ./client.out rcv 7 & wait
 
-x-terminal-emulator -e ./client.out rcv & wait
+x-terminal-emulator -e ./client.out rcv 8 & wait
 
-x-terminal-emulator -e ./client.out rcv & wait
+x-terminal-emulator -e ./client.out rcv 9 & wait
 
-x-terminal-emulator -e ./client.out rcv & wait
+x-terminal-emulator -e ./client.out rcv 10 & wait
--- main_folder/test_client_snd.ex
+++ main_folder/test_client_snd.ex
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-x-terminal-emulator -e ./client.out snd & wait
+x-terminal-emulator -e ./client.out snd 1 & wait
 
-x-terminal-emulator -e ./client.out snd & wait
+x-terminal-emulator -e ./client.out snd 2 & wait
 
-x-terminal-emulator -e ./client.out snd & wait
+x-terminal-emulator -e ./client.out snd 3 & wait
 
-x-terminal-emulator -e ./client.out snd & wait
+x-terminal-emulator -e ./client.out snd 4 & wait
 
-x-terminal-emulator -e ./client.out snd & wait
+x-terminal-emulator -e ./client.out snd 5 & wait
