ecd4b4a74bf7f6f649f2ec3c50b5ee516883e7e7
JamesKellerman <jfk535@mst.edu>
2014-12-03 21:10:38 -0600

    Script removes tracker and image files

--- main_folder/test_client_rcv.ex
+++ main_folder/test_client_rcv.ex
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+#remove tracker and image files
+rm -f test_clients/client_6/*.track 
+rm -f test_clients/client_6/*.jpg
+rm -f test_clients/client_7/*.track 
+rm -f test_clients/client_7/*.jpg
+rm -f test_clients/client_8/*.track 
+rm -f test_clients/client_8/*.jpg
+rm -f test_clients/client_9/*.track 
+rm -f test_clients/client_9/*.jpg
+rm -f test_clients/client_10/*.track 
+rm -f test_clients/client_10/*.jpg
+
+#run receiving client
 ./client.out rcv 6 &
 
 ./client.out rcv 7 &
