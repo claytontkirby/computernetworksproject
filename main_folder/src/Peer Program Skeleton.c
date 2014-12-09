0299a8c11c0d1116c5e24365f52d4f9165272136
JamesKellerman <jfk535@mst.edu>
2014-12-09 12:58:00 -0600

    Added comments for all functions

--- main_folder/src/Peer Program Skeleton.c
+++ main_folder/src/Peer Program Skeleton.c
@@ -18,8 +18,7 @@
 #include <vector>
 using namespace std;
 
-string sharedFilePath;
-string trackerFilePath;
+string sharedFilePath; //stores file path
 int MAX_RECV_LENGTH = 2000;
 int CLIENT_ID = 0;
 int CHUNK_SIZE = 0;
@@ -31,19 +30,20 @@ int R1 = 0;
 int R2 = 0; 
 string IPADDRESS = "127.0.0.1";
 string PORT = "3456";
-char THREAD1_RECVBUF[10000];
-char THREAD2_RECVBUF[10000];
-char THREAD3_RECVBUF[10000];
-char THREAD4_RECVBUF[10000];
-char THREAD5_RECVBUF[10000];
+char THREAD1_RECVBUF[10000]; //Buffer for first segment of received file
+char THREAD2_RECVBUF[10000]; //Buffer for second segment of received file
+char THREAD3_RECVBUF[10000]; //Buffer for third segment of received file
+char THREAD4_RECVBUF[10000]; //Buffer for fourth segment of received file
+char THREAD5_RECVBUF[10000]; //Buffer for fifth segment of received file
 int THREAD1_RECVSIZE = 0;
 int THREAD2_RECVSIZE = 0;
 int THREAD3_RECVSIZE = 0;
 int THREAD4_RECVSIZE = 0;
 int THREAD5_RECVSIZE = 0;
-pthread_mutex_t dwnld_lock;
-pthread_mutex_t connection_lock;
+pthread_mutex_t dwnld_lock; //mutex for file download
+pthread_mutex_t connection_lock; //mutex for TCP connection
 
+//Contains information related to each peer
 struct PeerInfo {
 	string ip;
 	string port;
@@ -53,6 +53,7 @@ struct PeerInfo {
 	string client_id;
 };
 
+//Contains information for tracker files
 struct TrackerFile {
 	string filename;
 	string filesize;
@@ -62,12 +63,7 @@ struct TrackerFile {
 	bool isNULL;
 };
 
-struct Config {
-	int port_num;
-	string ip_addr;
-	int update_time;
-};
-
+//Information regarding each thread
 struct ThreadParams {
 	string name;
 	string start_byte;
@@ -76,8 +72,6 @@ struct ThreadParams {
 	int sockid;
 	int threadid;	
 };
-
-Config configFile;
 
 void getWorkingDirectory();
 
@@ -111,6 +105,8 @@ int pthread_yield(void);
 
 bool sort_func(PeerInfo p1, PeerInfo p2) { return atoi(p1.client_id.c_str()) < atoi(p2.client_id.c_str()); }
 
+//Ensures that the correct parameters are passed to the program
+//and executes corresponding functionality for either sending or receiving clients
 int main(int argc, char *argv[]){	
 	CLIENT_ID = atoi(argv[2]);
 	getWorkingDirectory();
@@ -127,9 +123,10 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
+//Ensures that each receiving client's thread operates correctly
 void main_rcv() {
 	int sock_id;
-	pthread_t t1, t2, t3, t4, t5;
+	pthread_t t1, t2, t3, t4, t5; //threads for each receiving client
 	int prev_byte_1 = 0;
 	int prev_byte_2 = 7144;
 	int prev_byte_3 = 14288;
@@ -144,12 +141,14 @@ void main_rcv() {
 		sleep(5);
 	}	
 
+	//Initialize each buffer to zero
 	bzero(THREAD1_RECVBUF, 10000);
 	bzero(THREAD2_RECVBUF, 10000);
 	bzero(THREAD3_RECVBUF, 10000);
 	bzero(THREAD4_RECVBUF, 10000);
 	bzero(THREAD5_RECVBUF, 10000);
 
+	//Initialize mutexes for download and connection
 	pthread_mutex_init(&dwnld_lock, NULL);
 	pthread_mutex_init(&connection_lock, NULL);
 
@@ -162,6 +161,7 @@ void main_rcv() {
 			continue;			
 		}
 
+		//Increment over bytes
 		if(prev_byte_1 != atoi(tf.peerlist[0].end_byte.c_str()) &&
 			prev_byte_2 != atoi(tf.peerlist[1].end_byte.c_str()) &&
 			prev_byte_3 != atoi(tf.peerlist[2].end_byte.c_str()) &&
@@ -174,6 +174,7 @@ void main_rcv() {
 			prev_byte_4 = atoi(tf.peerlist[3].end_byte.c_str()) + 1;
 			prev_byte_5 = atoi(tf.peerlist[4].end_byte.c_str()) + 1;
 
+			//Update thread information
 			for(int i = 0; i < 5; i++) {
 				p[i].name = tf.filename;
 				p[i].start_byte = tf.peerlist[i].start_byte;
@@ -182,12 +183,14 @@ void main_rcv() {
 				p[i].threadid = i+1;
 			}
 
+			//Creates threads
 			pthread_create(&t1, NULL, run, &p[0]);
 			pthread_create(&t2, NULL, run, &p[1]);
 			pthread_create(&t3, NULL, run, &p[2]);
 			pthread_create(&t4, NULL, run, &p[3]);
 			pthread_create(&t5, NULL, run, &p[4]);
 
+			//Wait for threads to terminate
 			if(pthread_join(t1, NULL) == 0 &&
 			pthread_join(t2, NULL) == 0 &&
 			pthread_join(t3, NULL) == 0 &&
@@ -199,11 +202,13 @@ void main_rcv() {
 		sleep(1);		
 	}
 
+	//Uninitialize mutexes
 	pthread_mutex_destroy(&dwnld_lock);	
 	pthread_mutex_destroy(&connection_lock);						
 	writeToFile("picture-wallpaper.jpg");
 }
 
+//Sets up socket connections for sending clients and ensures advertising of correct file chunks
 void main_snd() {
 	int sock_id = setupConnections();
 	processCreateTrackerCommand(sock_id);	
@@ -211,6 +216,7 @@ void main_snd() {
 	for(int i = 0; i < 4; i++) {
 		calculateChunk(i);
 		cout << "I am client_" << CLIENT_ID << ", and I am advertising the following chunk of the file: ";
+		//calculate percentage based on chunk size and total file size
 		cout << ceil((CURRENT_CHUNK_BEGIN / float(TOTAL_FILE_SIZE))*100) + 1 << "% to "; 
 		cout << ceil((CURRENT_CHUNK_END / float(TOTAL_FILE_SIZE))*100)  << "%" << endl;
 		sock_id = setupConnections();
@@ -226,6 +232,7 @@ void *run(void *param) {
 	pthread_exit(0);
 }
 
+//Gets the current working directory to assist in correct file search and download
 void getWorkingDirectory() {
 	char cwd[100];	
 	stringstream ss;	
@@ -241,21 +248,24 @@ void getWorkingDirectory() {
 	sharedFilePath += "/";
 }
 
+//Sets up socket level connections between clients and tracker server
 int setupConnections() {
 	int sockid;
 	bool successful = false;
 	struct sockaddr_in server_addr;
 
-	if ((sockid = socket(AF_INET,SOCK_STREAM,0))==-1){//create socket
+	if ((sockid = socket(AF_INET,SOCK_STREAM,0))==-1){ //create socket
 		printf("socket cannot be created\n"); exit(0);
 	}
 
-    server_addr.sin_family = AF_INET;//host byte order
-    server_addr.sin_port = htons(3456);// convert to network byte order
+    server_addr.sin_family = AF_INET; //host byte order
+    server_addr.sin_port = htons(3456); // convert to network byte order
     server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-    pthread_mutex_lock(&connection_lock);
+
+    pthread_mutex_lock(&connection_lock); //lock connection mutex
+
     while(!successful) {
-	    if (connect(sockid ,(struct sockaddr *) &server_addr, sizeof(struct sockaddr))==-1){//connect and error check
+	    if (connect(sockid ,(struct sockaddr *) &server_addr, sizeof(struct sockaddr))==-1){ //connect and error check
 			cout << errno << endl; printf("Cannot connect to server\n");
 			if(errno == 22)
 				exit(1);
@@ -263,11 +273,12 @@ int setupConnections() {
 			successful = true;
 		}		
 	}
-	pthread_mutex_unlock(&connection_lock);
+	pthread_mutex_unlock(&connection_lock); //unlock connection mutex
 
 	return sockid;
 }
 
+//Creates tracker file
 void processCreateTrackerCommand(int sockid) {	
 	DIR* FD;
 	struct dirent* in_file;
@@ -312,7 +323,10 @@ void processCreateTrackerCommand(int sockid) {
 			strcat(funct_call, in_file->d_name);	//append file name to function call
 			FILE * pipe;
 			pipe = popen(funct_call, "r");	//call md5 on file
-			fgets(md5, 200, pipe);	//store output from md5sum call
+			
+			//Below call is different depending on operating system
+			//Mac: md5,  Linux: md5sum
+			fgets(md5, 200, pipe);	//store output from md5 call
 			strtok(md5, " ");
 			strtok(NULL, " ");
 			strtok(NULL, " ");
@@ -327,14 +341,14 @@ void processCreateTrackerCommand(int sockid) {
 			list_req += ss.str();
 
 			while(!createSuccessful) {
-				if((write(sockid, list_req.c_str(), list_req.size())) < 0){//inform the server of the list request
+				if((write(sockid, list_req.c_str(), list_req.size())) < 0){ //inform the server of the list request
 					printf("Send_request failure\n"); exit(0);
 				} else {
 					createSuccessful = true;
 				}
 			}
 
-		    if((length = recv(sockid, &msg, 100, 0) < 0)){// read what server has said
+		    if((length = recv(sockid, &msg, 100, 0) < 0)){ // read what server has said
 				printf("Read failure (create tracker) with error %d\n", errno); exit(0); 
 			}
 
@@ -348,11 +362,12 @@ void processCreateTrackerCommand(int sockid) {
 	close(sockid);
 }
 
+//Calculates the chunks of the file so that it can be sent in smaller segments over the network
 void calculateChunk(int iteration) {
 	int client_chunk_begin;
 
 	CHUNK_SIZE = TOTAL_FILE_SIZE / 5;
-	QUARTER_CHUNK_SIZE = CHUNK_SIZE / 4;
+	QUARTER_CHUNK_SIZE = CHUNK_SIZE / 4; 
 
 	client_chunk_begin = (QUARTER_CHUNK_SIZE * 4) * (CLIENT_ID - 1);
 	CURRENT_CHUNK_BEGIN = client_chunk_begin + (QUARTER_CHUNK_SIZE * iteration);
@@ -362,6 +377,7 @@ void calculateChunk(int iteration) {
 		CURRENT_CHUNK_END = TOTAL_FILE_SIZE; 
 }
 
+//Updates the tracker file
 void processUpdateTrackerCommand(int sockid) {
 	DIR* FD;
 	struct dirent* in_file;
@@ -413,7 +429,7 @@ void processUpdateTrackerCommand(int sockid) {
 			while(!updateSuccessful && totalBytesSent < list_req.size()) {
 				bytesSentThisTime = write(sockid, list_req.c_str(), list_req.size());
 				totalBytesSent += bytesSentThisTime;
-				if(bytesSentThisTime < 0){//inform the server of the list request
+				if(bytesSentThisTime < 0){ //inform the server of the list request
 					printf("Send_request failure\n");	
 					updateSuccessful = false;														
 				} else {
@@ -421,7 +437,7 @@ void processUpdateTrackerCommand(int sockid) {
 				}
 			}
 
-		    if((length = recv(sockid, &msg, 100, 0) < 0)){// read what server has said
+		    if((length = recv(sockid, &msg, 100, 0) < 0)){ // read what server has said
 				printf("Read failure (update tracker) with error %d\n", errno); exit(0); 
 			}
 
@@ -435,16 +451,17 @@ void processUpdateTrackerCommand(int sockid) {
 	close(sockid);
 }
 
+//Requests list from server and returns the list
 string processListCommand(int sockid) {
 	string list_req = "REQ LIST";
 	char msg[1001];
 	int length;
 
-	if((write(sockid, list_req.c_str(), list_req.size())) < 0){//inform the server of the list request
+	if((write(sockid, list_req.c_str(), list_req.size())) < 0){ //inform the server of the list request
 		printf("Send_request  failure\n"); exit(1);
 	}
 
-    if((length = read(sockid, &msg, 1000))< 0){// read what server has said
+    if((length = read(sockid, &msg, 1000))< 0){ // read what server has said
 		cout << errno << endl; printf("Read  failure\n"); exit(1); 
 	}
 	
@@ -454,10 +471,12 @@ string processListCommand(int sockid) {
 	return msg;
 }
 
+//Processes Get Command by downloading specified file
 void processGetCommand(int sockid, string filename, string start_byte, string end_byte, int threadid) {
 	downloadFile(filename, start_byte, end_byte, sockid, threadid);
 }
 
+//Requests tracker file from tracker server
 string requestTrackerFile(int sockid, string file) {
 	string list_req = "get ";
 	char fpath[300];
@@ -514,6 +533,7 @@ string requestTrackerFile(int sockid, string file) {
 	return fpath;
 }
 
+//Parse tracker file to update tracker information
 TrackerFile parseTrackerFile(string tfile, int prev_byte_1, int prev_byte_2, int prev_byte_3, int prev_byte_4, int prev_byte_5) {
 	ifstream in;
 	TrackerFile tf;
@@ -541,6 +561,7 @@ TrackerFile parseTrackerFile(string tfile, int prev_byte_1, int prev_byte_2, int
 		getline(in, pi.timestamp, ':');
 		getline(in, pi.client_id);
 
+		//1785 is the size of a chunk in bytes
 		if(!in.eof() && (
 			(atoi(pi.end_byte.c_str()) == prev_byte_1 + 1785) ||
 			(atoi(pi.end_byte.c_str()) == prev_byte_2 + 1785) ||
@@ -563,6 +584,7 @@ TrackerFile parseTrackerFile(string tfile, int prev_byte_1, int prev_byte_2, int
 	return tf;
 }
 
+//Downloads file
 void downloadFile(string filename, string start_byte, string end_byte, int sockid, int threadid) {
 	char recvBuf[10000];
 	int fd_block_size = 0;
@@ -573,7 +595,8 @@ void downloadFile(string filename, string start_byte, string end_byte, int socki
 	string download_req = "download " + filename + " " + start_byte + " " + end_byte + " " + ss.str();	
 
 
-	pthread_mutex_lock(&dwnld_lock);
+	pthread_mutex_lock(&dwnld_lock); //lock download mutex
+
 	while(downloadInProgress) {
 
 		if((write(sockid, download_req.c_str(), download_req.size())) < 0) {
@@ -642,9 +665,10 @@ void downloadFile(string filename, string start_byte, string end_byte, int socki
 		close(sockid);
 		sockid = setupConnections();
 	}
-	pthread_mutex_unlock(&dwnld_lock);
+	pthread_mutex_unlock(&dwnld_lock); //unlock download mutex
 }
 
+//Outputs downloaded information from buffer to actual file
 void writeToFile(string filename) {
 	char fpath[300];
 	strcpy(fpath, sharedFilePath.c_str());
@@ -666,5 +690,3 @@ void writeToFile(string filename) {
 	rewind(fd);
 	fclose(fd);
 }
-
-///@file
